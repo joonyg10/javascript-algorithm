df3c17f707587a44bca62edf89446e8a769afe7f
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-03-28 20:05:13 +0900

    Refactor: 타겟넘버 정리

--- programmers/Kakao 기출/03_타겟넘버.ts
+++ programmers/Kakao 기출/03_타겟넘버.ts
@@ -1,24 +1,24 @@
-function kakao_solution_4(numbers: number[], target: number) {
-  const N: number = numbers.length;
+// [문제]: numbers 사이사이에 +, -를 넣어 target를 만들 수 있는 가짓수 구하기
+
+function kakao_solution_3(numbers: number[], target: number) {
+  const lastIdx: number = numbers.length - 1;
   let answer = 0;
 
-  function backTrack(currIdx: number, sum: number) {
-    if (currIdx === N - 1) {
-      if (sum === target) answer += 1;
+  // currIdx = -1, acc: 0 부터 시작
+  function backTrack(currIdx: number, acc: number) {
+    if (currIdx === lastIdx) {
+      if (acc === target) answer += 1;
       return;
     }
 
-    const nextIdx = currIdx + 1;
     for (const sign of [-1, 1]) {
-      const nextSum = sum + sign * numbers[nextIdx];
-      backTrack(nextIdx, nextSum);
+      const nextSum = acc + sign * numbers[currIdx + 1];
+      backTrack(currIdx + 1, nextSum);
     }
   }
-
-  backTrack(0, numbers[0]);
-  backTrack(0, -numbers[0]);
+  backTrack(-1, 0);
   return answer;
 }
 
-console.log(kakao_solution_4([1, 1, 1, 1, 1], 3));
-console.log(kakao_solution_4([4, 1, 2, 1], 4));
+console.log(kakao_solution_3([1, 1, 1, 1, 1], 3)); // 5
+console.log(kakao_solution_3([4, 1, 2, 1], 4)); // 2
